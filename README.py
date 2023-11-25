706c1ed892c6c4171a7bfbfe350bada869a66591
Kn772 <151584422+Kn772@users.noreply.github.com>
2023-11-25 11:40:49 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,19 @@
 # Trabalho-espa-o-
+
+import cx_Freeze
+executables = [
+    cx_Freeze.Executable(script="main.py", icon="icospace.ico")
+]
+cx_Freeze.setup(
+    name = "SpaceMarker",
+    options = {
+        "build_exe":{
+            "packages": ["pygame"],
+            "include_files": [
+                "fundospace.jpg",
+                "space.png",
+                "Space_Machine_Power.mp3"
+            ]
+        }
+    } , executables = executables
+)
